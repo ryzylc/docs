8207a03d68ae2bbeb5bb9660c06c3616156625de
bigbigli <bigbigli@yeah.net>
2025-11-03 15:53:33 +0800

    删除编辑此页

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -38,9 +38,9 @@ export default hopeTheme({
   },
 
   // 多语言配置
-  metaLocales: {
-    editLink: "编辑此页",
-  },
+  // metaLocales: {
+  //   editLink: "编辑此页",
+  // },
 
   // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
   // hotReload: true,
